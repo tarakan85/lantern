13b05df9fae87b413062faa5434814b31b8ecb9e
Nykolai Topchyi <nodetempest@gmail.com>
2023-02-19 20:24:34 +0200

    implement iridescent color state

--- src/components/lantern/lantern.component.tsx
+++ src/components/lantern/lantern.component.tsx
@@ -1,7 +1,13 @@
-import React from "react";
 import { Box, IconButton } from "@mui/material";
 import PowerSettingsNewSharpIcon from "@mui/icons-material/PowerSettingsNewSharp";
-import { yellow, red } from "@mui/material/colors";
+import {
+  yellow,
+  red,
+  lightGreen,
+  blue,
+  purple,
+  green,
+} from "@mui/material/colors";
 
 import { useRedBlueFlicker, useRedFlicker } from "./hooks";
 import { useLantern } from "~/state/lantern/lantern.hooks";
@@ -31,7 +37,6 @@ export const Lantern = () => {
     colorfulRedStatic: red[500],
     colorfulRedFlicker: redFlickerColor,
     colorfulRedBlueFlicker: redBlueFlickerColor,
-    colorfulIridescent: "aqua",
   };
 
   const color = lantern.state.isTurnedOn ? colorMap[modesCombo] : "grey.50";
@@ -50,13 +55,41 @@ export const Lantern = () => {
         sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
       >
         <Box
-          sx={{
-            border: "1px solid",
-            borderColor: "grey.700",
-            borderRadius: "8px",
-            padding: "90px 80px",
-            bgcolor: color,
-          }}
+          sx={[
+            {
+              border: "1px solid",
+              borderColor: "grey.700",
+              borderRadius: "8px",
+              padding: "90px 80px",
+              bgcolor: color,
+            },
+            modesCombo === "colorfulIridescent" && {
+              animationName: "backgroundColorPalette",
+              animationDuration: "8s",
+              animationIterationCount: "infinite",
+              animationDirection: "alternate",
+              "@keyframes backgroundColorPalette": {
+                "0%": {
+                  background: lightGreen["A700"],
+                },
+                "20%": {
+                  background: blue[500],
+                },
+                "40%": {
+                  background: purple[500],
+                },
+                "60%": {
+                  background: red[500],
+                },
+                "80%": {
+                  background: yellow[700],
+                },
+                "100%": {
+                  background: green[700],
+                },
+              },
+            },
+          ]}
         />
         <Box
           sx={{
