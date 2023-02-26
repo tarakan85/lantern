f86afe81c8f05fad5b470827c68bdfb1509caad6
Nykolai Topchyi <nodetempest@gmail.com>
2023-02-26 18:47:04 +0200

    reorganise export for lantern component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { Provider } from "react-redux";
 
-import { Lantern } from "~/components/lantern";
+import { Lantern } from "~/components/lantern/lantern.component";
 import { store } from "~/state/root/root.store";
 
 export const App = () => {
